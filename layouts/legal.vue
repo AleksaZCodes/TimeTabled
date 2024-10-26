1492245df9d77049df103b1e89c123e0b44f0039
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-10-26 12:00:00 +0200

    Bugfix

--- layouts/legal.vue
+++ layouts/legal.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="mx-auto flex max-w-2xl flex-col gap-6 py-10">
+  <main class="mx-auto flex max-w-2xl flex-col gap-6 p-4 py-10">
     <NuxtLink class="btn btn-ghost w-fit" to="/">
       <FontAwesome class="text-lg" icon="arrow-left" /> Back to app
     </NuxtLink>
